5551aef70a6da4f2664b84855958ba03d74b4680
EC2 Default User <ec2-user@ip-172-31-44-152.ap-northeast-1.compute.internal>
2020-06-05 04:24:59 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      log_in @user
+      redirect_to @user
     else
       render 'new'
     end
+  end
+  
+  def show
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login' => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
   post '/signup' => 'users#create'
   get '/signup' =>  "users#new"
 
