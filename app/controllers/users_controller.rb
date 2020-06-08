36e1d67d1b9d1bef5aedef4886a21ea7e8749188
EC2 Default User <ec2-user@ip-172-31-44-152.ap-northeast-1.compute.internal>
2020-06-08 08:29:05 +0000

    update users on the way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
@@ -7,15 +12,29 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      flash[:success] = "ユーザーを作成しました。"
       redirect_to @user
     else
+      flash[:danger] = "ユーザーの保存に失敗しました。"
       render 'new'
     end
   end
   
-  def show
+  def edit
+    @user = User.find(params[:id])
   end
-
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "変更を保存しました"
+      redirect_to @user
+    else
+      flash.now[:dager] = "変更の保存に失敗しました"
+      render "edit"
+    end
+  end
+  
   private
 
     def user_params
