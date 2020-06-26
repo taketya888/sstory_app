5682007187aab97d16c9844822a6f38bb83b6ccd
EC2 Default User <ec2-user@ip-172-31-44-152.ap-northeast-1.compute.internal>
2020-06-26 06:58:49 +0000

    CSS_custom

--- app/models/story.rb
+++ app/models/story.rb
@@ -14,7 +14,7 @@ class Story < ApplicationRecord
   def save_categories(tags)
     current_tags = self.categories.pluck(:name) unless self.categories.nil?
     return if current_tags.blank?
-    return if tags.blank?
+    #return if tags.blank?
     old_tags = current_tags - tags
     new_tags = tags - current_tags
     
