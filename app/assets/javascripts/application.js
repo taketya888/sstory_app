1a66d23c4e4855dd1d7adf30e71cc7f62a8e0035
EC2 Default User <ec2-user@ip-172-31-44-152.ap-northeast-1.compute.internal>
2020-07-14 08:34:10 +0000

    CSS_custom_end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require tag-it
 //= require rails-ujs
 //= require bootstrap
+//= require pagetop
 //= require headrock
 //= require_tree .
 //= require turbolinks
---
+++ app/assets/javascripts/pagetop.js
@@ -0,0 +1,19 @@
+(function($) {
+document.addEventListener("turbolinks:load", function() {
+  var offset = 100;
+  var duration = 500;
+  $(window).scroll(function() {
+  if ($(this).scrollTop() > offset) {
+  $('.pagetop').fadeIn(duration);
+  } else {
+  $('.pagetop').fadeOut(duration);
+  }
+  });
+  
+  $('.pagetop').click(function(event) {
+  event.preventDefault();
+  $('html, body').animate({scrollTop: 0}, duration);
+  return false;
+  })
+})
+})(jQuery);
